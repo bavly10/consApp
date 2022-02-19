c9b47c5149601cc60bcdeb03662538b96628e242
siham <siham1554@gmail.com>
2022-02-19 12:11:49 +0300

    adsss

---
+++ lib/payment/pay_errors/pay_errors.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:helpy_app/model/user_model.dart';
+import 'package:helpy_app/modules/User_screen/introducer.dart';
+import 'package:helpy_app/shared/componotents.dart';
+import 'package:helpy_app/shared/localization/translate.dart';
+import 'package:helpy_app/shared/my_colors.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class PaymentError extends StatelessWidget {
+  final UserStrapi cubit;
+  final dynamic mytext;
+  const PaymentError({Key? key, required this.cubit,this.mytext}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:  HexColor('#eff1f3'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              "assets/15_Payment Error.png",
+             fit: BoxFit.cover,
+             height: MediaQuery.of(context).size.height*0.50,
+             width: double.infinity,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(child: Text(mytext,style:  const TextStyle(fontSize:18.0,color: Colors.blueGrey),textAlign: TextAlign.center,)),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 13),
+                    blurRadius: 25,
+                    color: Color(0xFF5666C2).withOpacity(0.17),
+                  ),
+                ],
+              ),
+              child: FlatButton(
+                color: HexColor('#C18F3A'),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50)),
+                onPressed: () {
+                  navigateToFinish(context, Introducer(cubit.id!));
+                },
+                child: Text(
+                  mytranslate(context, "back"),
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/payment/payment.dart
+++ lib/payment/payment.dart
@@ -5,57 +5,57 @@ import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:helpy_app/modules/Deatils_Special/cubit/cubit.dart';
 import 'package:helpy_app/modules/Deatils_Special/cubit/states.dart';
 import 'package:helpy_app/modules/customer/Chat/chats_screen.dart';
+import 'package:helpy_app/payment/pay_errors/pay_errors.dart';
 import 'package:helpy_app/shared/componotents.dart';
 
 class PaymentsTest extends StatelessWidget {
   final String url;
   final int id;
-  PaymentsTest(this.url,this.id);
-  ScrollController scrollController= ScrollController();
+  PaymentsTest(this.url, this.id);
+  ScrollController scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
     final cubit = ConsCubitIntro.get(context).findbyid(id);
-    return BlocBuilder<ConsCubitIntro,cons_StatesIntro>(
-      builder:(ctx,state){
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("Going Chat with ${cubit.username}"),
-            backgroundColor: Colors.white,
-            elevation: 0,
+    return BlocBuilder<ConsCubitIntro, cons_StatesIntro>(builder: (ctx, state) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text("Going Chat with ${cubit.username}"),
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          controller: scrollController,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 1,
+            child: InAppWebView(
+              initialUrlRequest: URLRequest(url: Uri.parse(url)),
+              initialOptions: InAppWebViewGroupOptions(
+                crossPlatform: InAppWebViewOptions(
+                  allowFileAccessFromFileURLs: true,
+                  allowUniversalAccessFromFileURLs: true,
+                  useOnLoadResource: true,
+                  javaScriptEnabled: true,
+                ),
+              ),
+              onLoadStop: (InAppWebViewController controller, url) async {
+                await controller.evaluateJavascript(source: "document.documentElement.innerText")
+                    .then((value) {
+                  if (value.toString().contains("Transaction not completed") ||
+                      value.toString().contains("لم تكتمل المعاملة")) {
+                    navigateToFinish(context, PaymentError(cubit: cubit,mytext: value,));
+                  } else if (
+                  value.toString().contains("Transaction successful") ||
+                      value.toString().contains("معاملة ناجحة")) {
+                    navigateToFinish(context, ChatsScreen());
+                  } else {
+                    null;
+                  }
+                });
+              },
+            ),
           ),
-          body: SingleChildScrollView(
-            controller: scrollController,
-           child: SizedBox(
-                height: MediaQuery.of(context).size.height *1,
-                child: InAppWebView(
-                  initialUrlRequest: URLRequest(url: Uri.parse(url)),
-                  initialOptions: InAppWebViewGroupOptions(
-                    crossPlatform: InAppWebViewOptions(
-                      allowFileAccessFromFileURLs: true,
-                      allowUniversalAccessFromFileURLs: true,
-                      useOnLoadResource: true,
-                      javaScriptEnabled: true,
-                    ),
-                  ),
-                  onLoadStop: (InAppWebViewController controller, url)async{
-                    await controller.evaluateJavascript(source: "document.documentElement.innerText").then((value) {
-                      if(value.toString().contains("Transaction not completed")){
-                        scrollController.jumpTo(scrollController.position.minScrollExtent);
-                        myToast(message: "Transaction not completed");
-                        navigateToFinish(context, "PaymentError(cubit)");
-                      }else if (value.toString().contains("Transaction successful")){
-                        scrollController.jumpTo(scrollController.position.minScrollExtent);
-                        navigateToFinish(context, ChatsScreen());
-                      }else{null;}
-                    });
-                  },
-                )
-                ,),
-          ),
-        );
-      }
-    );
+        ),
+      );
+    });
   }
-
-
 }
